9811dead48b0caa730a40b62998a0d589b18cfaf
Rasal Hossain Adib <adibrasel.2022@gmail.com>
2023-04-28 14:57:32 +0600

    Remittance Calculation
    Remittance Calculation

--- script.js
+++ script.js
@@ -67,16 +67,32 @@ function RemittanceCalculation(){
     let Interest = RemittanceTaka * InterestRate / 100;
     let Total_Taka = RemittanceTaka + Interest
 
-
     document.getElementById("Total_Remittance").innerHTML = RemittanceTaka
     document.getElementById("Total_Interest").innerHTML = Interest
     document.getElementById("Total_Taka").innerHTML = Total_Taka
-
-
-
-
+    document.getElementById("Spelling").innerHTML =inWords(Total_Taka)
 
 }
+
+
+var a = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+var b = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety' ];
+
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'Overflow';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + ' Crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + ' Lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + ' Thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + ' Hundred ' : '';
+    str += (n[5] != 0) ? ((str != ' ') ? ' ' : ' ') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + ' Only ' : '';
+    return str;
+}
+
+
+
+
 
 
 
